254fe1b274908a2a025f732218d34b55e477a921
andy glessner <andyglessner@hotmail.com>
2018-11-03 20:32:28 -0400

    initial thoughts

--- TernaryTree/TernaryTreeSearch.cs
+++ TernaryTree/TernaryTreeSearch.cs
@@ -9,8 +9,9 @@ namespace TernaryTree
 
     public class TernaryTreeSearch<V>
     {
+        private delegate int Transition(char c);
+        private List<Transition> _transitions;
         private int _state;
-        private Func<int>[] _stateImplementations;
         // Delegates are MatchRange, MatchExact, MatchAny, etc.
         // They return bool (whether a match was made) - so you know whether to send
         // the machine down the branch.
@@ -25,6 +26,13 @@ namespace TernaryTree
             // Build a state machine by filling the _stateImplementations array with delegates.
             // Need to define my delegates below so that they can be plugged in with values.
             // Just need to think of all delegates to handle every type of transition / edge.
+            // Example of how to add an edge to the state machine:
+            // _transitions[0] += _matchRange(pattern[0], pattern[3], 1, 0) as Transition;
+            // Alternatively, _transitions could be a List<List<Transition>> and we could do:
+            // _transitions[0][0] = new Transition(_matchRange(pattern[0], pattern[3], 1, 0));
+            // _transitions[0][1] = new Transition(_matchExact(pattern[1], 1, 0);
+            // And then an iteration of Match() would go:
+            // foreach (Transition t in _transitions[_state]) t.Invoke(Node.Value);
         }
 
         /// <summary>
@@ -34,11 +42,53 @@ namespace TernaryTree
         /// <returns></returns>
         public ICollection<string> Match(Node<V> node)
         {
-            // This method should actually return a bool
-            // and accept an int for state, and a char for the character to evaluate.
-            // update state and return yes or no for whether a match was made.
-            // Ternary tree needs a _matchPattern method to drive it.
+            // Make a private method to 
+            // recursively invoke state, make a copy of this state machine,
+            // and send the copy down branches as instructed by the result?
+            // Or write the delegates to handle that?
             throw new NotImplementedException();
+        }
+
+        private Func<char, int> _matchRange(char a, char b, int successState, int failureState)
+        {
+            Func<char, int> f = c =>
+            {
+                if (c >= _getMinChar(a, b) && c <= _getMaxChar(a, b))
+                {
+                    // Maybe we should handle cloning the state machine
+                    // and sending it down the branch here?
+                    return successState;
+                }
+                else
+                {
+                    return failureState;
+                }
+            };
+            return f;
+        }
+        
+        private char _getMinChar(char a, char b)
+        {
+            if (a <= b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+
+        private char _getMaxChar(char a, char b)
+        {
+            if (a <= b)
+            {
+                return b;
+            }
+            else
+            {
+                return a;
+            }
         }
     }
 }
