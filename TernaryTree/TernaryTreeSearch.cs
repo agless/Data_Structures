ab7b2343842836ec14979e898d985c473039f79c
andy glessner <andyglessner@hotmail.com>
2018-11-11 22:39:17 -0500

    first steps toward constructing the state machine

--- TernaryTree/TernaryTreeSearch.cs
+++ TernaryTree/TernaryTreeSearch.cs
@@ -19,9 +19,13 @@ namespace TernaryTree
         /// <param name="pattern"></param>
         public TernaryTreeSearch(string pattern)
         {
+            if (string.IsNullOrEmpty(pattern))
+            {
+                throw new ArgumentNullException(nameof(pattern));
+            }
             // Build a state machine by filling the _stateImplementations array with delegates.
             // Need to define my delegates below so that they can be plugged in with values.
-            // Just need to think of all delegates to handle every type of transition / edge.
+            // Just need to provide all delegates to handle every type of transition / edge.
             // Each state can have more than one transition, each leading to a different state.
             //
             // Example of how to add an edge to the state machine:
@@ -31,12 +35,9 @@ namespace TernaryTree
             // Should account for prefix match case.  It seems more efficient to
             // grab all keys from a prefix branch at once than to go through the state check/set
             // rigamarole.
-        }
 
-        private TernaryTreeSearch(List<List<Transition>> transitions, int state)
-        {
-            _transitions = transitions;
-            _state = state;
+            // Kick off the state building process
+            _buildState(0, pattern);
         }
 
         /// <summary>
@@ -46,51 +47,12 @@ namespace TernaryTree
         /// <returns></returns>
         public ICollection<string> Match(Node<V> node)
         {
-            // Make a private method to 
-            // recursively invoke state, make a copy of this state machine,
-            // and send the copy down branches as instructed by the result?
-            // Or write the delegates to handle that?
             ICollection<string> matches = new LinkedList<string>();
             _getBranchMatches(node, default(string), matches);
             return matches;
         }
 
-        private void _getBranchMatches(Node<V> node, string key, ICollection<string> matches)
-        {
-            if (_state >= _transitions.Count)
-            {
-                return;
-            }
-            if (node.Smaller != null)
-            {
-                TernaryTreeSearch<V> tts = new TernaryTreeSearch<V>(_transitions, _state);
-                tts._getBranchMatches(node.Smaller, key, matches);
-            }
-            StringBuilder oldString = new StringBuilder(key.ToString());
-            int oldState = _state;
-            string newKey = key + node.Value;
-            foreach (Transition transition in _transitions[_state])
-            {
-                int nextState = transition.Invoke(node.Value);
-                if (nextState > -1)
-                {
-                    if (nextState == _transitions.Count && node.IsFinalNode)
-                    {
-                        matches.Add(newKey);
-                    }
-                    if (nextState < _transitions.Count && node.Equal != null)
-                    {
-                        _state = nextState;
-                        _getBranchMatches(node.Equal, newKey, matches);
-                        _state = oldState;
-                    }
-                }
-            }
-            if (node.Bigger != null)
-            {
-                _getBranchMatches(node.Bigger, key, matches);
-            }
-        }
+        #region Delegatees
 
         private Func<char, int> _matchEverything(int successState)
         {
@@ -158,6 +120,117 @@ namespace TernaryTree
             return f;
         }
 
+        #endregion
+
+        #region Private Methods
+
+        private TernaryTreeSearch(List<List<Transition>> transitions, int state)
+        {
+            _transitions = transitions;
+            _state = state;
+        }
+
+        private void _buildState(int pos, string pattern)
+        {
+            while (pos < pattern.Length)
+            {
+                char c = pattern[pos];
+                switch (c)
+                {
+                    case '\\':
+                        pos = _handleEscape(pos, pattern);
+                        continue;
+                    case '^': // TODO: Write private methods for each of these special characters.
+                    case '$':
+                    case '.':
+                    case '|':
+                    case '?':
+                    case '*':
+                    case '+':
+                        pos = _handleLiteral(pos, pattern);
+                        continue;
+                    case '(':
+                    case '[':
+                    case '{':
+                        // TODO: Handle grouping.
+                        // pos = _handleGroup(pos, pattern);
+                        continue;
+                    case ']':
+                    case ')':
+                    case '}':
+                    default:
+                        pos = _handleLiteral(pos, pattern);
+                        break;
+                }
+            }
+        }
+
+        private int _handleLiteral(int pos, string pattern)
+        {
+            char c = pattern[pos++];
+            Transition t = new Transition(_matchExact(c, _transitions.Count));
+            _transitions[_state].Add(t);
+            int _oldState = _state;
+            _state = _transitions.Count;
+            _buildState(pos, pattern);
+            _state = _oldState;
+            return pos;
+        }
+
+        private int _handleEscape(int pos, string pattern)
+        {
+            if (pos + 1 >= pattern.Length)
+            {
+                // An escape character was passed as the last character in a string.
+                // Could just ignore?
+                // Otherwise:
+                // _handleSyntaxError()
+            }
+            else
+            {
+                // TODO: Call the right method for the escaped character.
+                // Switch statement?
+            }
+            return pos;
+        }
+
+        private void _getBranchMatches(Node<V> node, string key, ICollection<string> matches)
+        {
+            if (_state >= _transitions.Count)
+            {
+                return;
+            }
+            if (node.Smaller != null)
+            {
+                TernaryTreeSearch<V> tts = new TernaryTreeSearch<V>(_transitions, _state);
+                tts._getBranchMatches(node.Smaller, key, matches);
+            }
+            StringBuilder oldString = new StringBuilder(key.ToString());
+            int oldState = _state;
+            string newKey = key + node.Value;
+            foreach (Transition transition in _transitions[_state])
+            {
+                int nextState = transition.Invoke(node.Value);
+                if (nextState > -1)
+                {
+                    if (nextState == _transitions.Count && node.IsFinalNode)
+                    {
+                        matches.Add(newKey);
+                    }
+                    if (nextState < _transitions.Count && node.Equal != null)
+                    {
+                        _state = nextState;
+                        _getBranchMatches(node.Equal, newKey, matches);
+                        _state = oldState;
+                    }
+                }
+            }
+            if (node.Bigger != null)
+            {
+                _getBranchMatches(node.Bigger, key, matches);
+            }
+        }
+
         private char _getMinChar(char a, char b)
         {
             if (a <= b)
@@ -181,5 +254,7 @@ namespace TernaryTree
                 return a;
             }
         }
+
+        #endregion
     }
 }
