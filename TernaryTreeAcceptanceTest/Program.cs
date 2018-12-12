5bae2c81badd83e846c83d53316a6b9d844163b9
Andy Glessner <agless@users.noreply.github.com>
2018-12-11 22:17:06 -0500

    initial commit

---
+++ TernaryTreeAcceptanceTest/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using TernaryTree;
+
+namespace TernaryTreeAcceptanceTest
+{
+    class Program
+    {
+        private static string[] tenants = new string[]
+        {
+            "ABC Co.",
+            "XYZ Co.",
+            "The Great Parcheisi",
+            "The Group, LLC",
+            "Dingleberg, Inc."
+        };
+
+        private static string[] docTypes = new string[]
+        {
+            "photo_id",
+            "passport",
+            "invoice",
+            "receipt",
+            "customer_history",
+            "correspondence"
+        };
+
+        static void Main(string[] args)
+        {
+            long startTime = default(long);
+            long endTime = default(long);
+            List<string> keys = new List<string>();
+            foreach (string tenant in tenants)
+            {
+                Console.Write($"Generating 10,000 keys for tenant {tenant}.");
+                Random random = new Random();
+                startTime = DateTime.Now.Ticks;
+                for (int i = 0; i < 10000; i++)
+                {
+                    int docType = random.Next(0, docTypes.Length);
+                    string key = $"{tenant}.{docTypes[docType]}.{Guid.NewGuid()}";
+                    keys.Add(key);
+                }
+                endTime = DateTime.Now.Ticks;
+                Console.Write($"Took {(endTime - startTime) / 10000} ms." + Environment.NewLine + Environment.NewLine);
+            }
+
+            Console.Write("Adding all keys to TernaryTree.");
+            startTime = DateTime.Now.Ticks;
+            TernaryTree<int> subject = TernaryTree<int>.Create(keys);
+            endTime = DateTime.Now.Ticks;
+            Console.WriteLine($"Took {(endTime - startTime) / 10000} ms." + Environment.NewLine + Environment.NewLine);
+
+            Console.Write("Finding all invoices for ABC Co.: \"ABC Co\\..*\" " + Environment.NewLine);
+            string pattern = @"ABC Co\..*";
+            Console.WriteLine("Running System regex across list.");
+            startTime = DateTime.Now.Ticks;
+            int count = 0;
+            foreach (string key in keys)
+            {
+                if (Regex.IsMatch(key, pattern))
+                {
+                    count++;
+                }
+            }
+            endTime = DateTime.Now.Ticks;
+            Console.WriteLine($"Found {count} keys in {(endTime - startTime) / 10000} ms." + Environment.NewLine);
+            Console.WriteLine("Running TernaryTreeSearch.");
+            startTime = DateTime.Now.Ticks;
+            ICollection<string> results = subject.Match(pattern);
+            endTime = DateTime.Now.Ticks;
+            Console.WriteLine($"Found {results.Count} keys in {(endTime - startTime) / 10000} ms." + Environment.NewLine + Environment.NewLine);
+
+            Console.ReadLine();
+        }
+    }
+}
